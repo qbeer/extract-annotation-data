8a822847f5978055251aa840058d322e54329c11
Alex Olar <olaralex@phys-gs.elte.hu>
2019-08-13 14:16:32 +0200

    finished annotation extraction, moving on to qupaht conversion

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from src import acquire_image_ids, acquire_annotations
+import json
 
-acquire_image_ids()
-acquire_annotations()
+elte_id_with_polygon_data = acquire_annotations()
+
+with open('annotations.json', 'w+') as final_annotations_file:
+    json.dump(elte_id_with_polygon_data, final_annotations_file)
+
+print('Written to file!')
